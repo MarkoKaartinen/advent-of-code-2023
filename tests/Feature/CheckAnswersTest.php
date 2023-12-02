bde5ab07c4f899503f85ea4129c3b5ab14e4ebcd
Marko Kaartinen <marko@elysium.fi>
2023-12-02 09:02:59 +0200

    Add Day 2 to CheckAnswersTest also

--- tests/Feature/CheckAnswersTest.php
+++ tests/Feature/CheckAnswersTest.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Days\Day1;
+use App\Days\Day2;
 
 test('Day 1', function () {
     $this->assertEquals(55477, (new Day1)->part1());
     $this->assertEquals(54431, (new Day1)->part2());
+});
+
+test('Day 2', function () {
+    $this->assertEquals(2913, (new Day2)->part1());
+    $this->assertEquals(55593, (new Day2)->part2());
 });
